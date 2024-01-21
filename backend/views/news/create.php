9ea3079e477250cc4a7913bb7a6c7cdd4ceb41a4
lwy <3428862097@qq.com>
2024-01-21 18:51:16 +0800

    add 5

---
+++ backend/views/news/create.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Team:lllg,NKU
+ * coding by 李秉睿 2113087
+ * gii生成
+ */
+
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\CovNews */
+
+$this->title = 'Create News';
+$this->params['breadcrumbs'][] = ['label' => 'News', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="cov-news-create">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
